466bfbd5420ca15ff9ac1b7d8fb63b6701e97d78
gui-gng <30386748+gui-gng@users.noreply.github.com>
2019-10-02 17:26:46 +1000

    Build

---
+++ build/precache-manifest.7dbd11214d780ab07a2fab7da363fb4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7edaf4340c8a49fac44d1b72056bc13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "421fe9d061ff64eec448",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "744ebe5fcf5839299b47",
+    "url": "/static/js/2.1b3372cf.chunk.js"
+  },
+  {
+    "revision": "421fe9d061ff64eec448",
+    "url": "/static/js/main.febd1ac4.chunk.js"
+  },
+  {
+    "revision": "8d11d355546751753761",
+    "url": "/static/js/runtime-main.be2d79cf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
