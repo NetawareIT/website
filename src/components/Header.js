9a6201f41031cea2713619fc8bcfcbd14bbb980c
gui-gng <30386748+gui-gng@users.noreply.github.com>
2019-10-23 16:53:24 +1000

    Update Header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => (
+    <div className="w-100 d-flex  align-items-center justify-content-between p-3 bg-blue-Netaware">
+        <div>
+            <img src="./logo.png" />
+        </div>
+        <div>
+            Netaware
+        </div>
+        <a class="btn btn-sm rounded border bg-dark text-white" title="Quick Support" href="./public/resources/TeamViewer.exe">
+        <span class="fa fa-desktop"></span> Quick Support
+    </a>
+    </div>
+)
