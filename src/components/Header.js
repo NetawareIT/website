7387b6ed9bc610291faddc3bea61467725019f13
gui-gng <30386748+gui-gng@users.noreply.github.com>
2019-11-20 19:10:17 +1000

    Changes Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDesktop } from '@fortawesome/free-solid-svg-icons';
 
+import './Header.css';
+
+
 export default () => (
-    <div className="w-100 d-flex  align-items-center justify-content-between p-2 bg-blue-Netaware">
+    <div className="w-100 d-flex  align-items-center justify-content-between p-2 header-bg">
         <div>
             <img className="pl-2" src="./logox30.png" />
         </div>
+        
         <div className="text-white d-flex  align-items-center">
-            <div className="pr-3 pl-3 netaware-pointer">
+            <div className="pr-3 pl-3 netaware-pointer active">
                 ABOUT US
             </div>
             <div className="pr-3 pl-3 netaware-pointer">
@@ -19,7 +23,7 @@ export default () => (
             </div>
         </div>
         <div>
-            <a class="btn btn-sm rounded border bg-dark text-white d-flex  align-items-center justify-content-between" title="Quick Support" href="./public/resources/TeamViewer.exe">
+            <a class="btn btn-sm rounded  bg-dark text-white d-flex  align-items-center justify-content-between v-desktop" title="Quick Support" href="./public/resources/TeamViewer.exe">
                 <FontAwesomeIcon icon={faDesktop} /> <div className="pl-3"> Quick Support</div>
             </a>
         </div>
